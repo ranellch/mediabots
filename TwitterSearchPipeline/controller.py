3431b9aa3ec88792ecb818c3045d76b0c45070e4
Christopher Ranella <chrisranella@gmail.com>
2016-12-18 14:53:34 -0600

    rest controller refactoring

--- TwitterSearchPipeline/controller.py
+++ TwitterSearchPipeline/controller.py
@@ -7,98 +7,142 @@ config.read('../bot.config')
 
 # it's about time to create a TwitterSearch object with our secret tokens
 # information from C.Ranella botnet1 app on twitter (he has actual access but these codes will work)
-ts = TwitterSearch(
-    consumer_key = config['TwitterAuth']['consumer_key'],
-    consumer_secret = config['TwitterAuth']['consumer_secret'],
-    access_token = config['TwitterAuth']['access_token'],
-    access_token_secret = config['TwitterAuth']['access_token_secret']
-)
 
-last_max_id = int(config['TwitterSearchQuery']['last_max_id'])
 
-tso = TwitterSearchOrder()
-if last_max_id != 0:
-    tso.set_since_id(last_max_id)
 
 class TwitterSearchQuery:
-    def __init__(self, ts, tso):
+    def __init__(self, ts, dict, dbKey):
         self.ts = ts
-        self.tso = tso
+        self.tso = TwitterSearchOrder()
         self.last_max_id = 0
-    
-    def setBasicSearchParameters(self, keywordList):
+        self.dbKey = dbKey
         self.tso.remove_all_filters()
-        self.tso.set_keywords(keywordList) # let's define all words we would like to have a look for
-        self.tso.set_language('en') # we want to see English tweets only
-        self.tso.set_include_entities(False) # and don't give us all those entity information
+        self.tso.set_language('en')
+        self.tso.set_include_entities(False)
         self.tso.set_count(100)
+        self.buffer = ['default']
+        self.queriesAllowed = 10
+        
+        for key in dict.keys():
+            if (key == "keywords"):
+                self.tso.set_keywords(dict["keywords"])
+            # add more parameters here...create a global resource for all possible parameters
+
+    def setSinceId(self, last_max_id):
+        self.last_max_id = last_max_id
+        self.tso.set_since_id(last_max_id)
         
     def getCurrentSearchURL(self):
-        return self.tso.create_search_url()    
+        return self.tso.create_search_url()  
         
-    def performSearch(self, buffer):
-        buffer.clear()
+    def performSearch(self):
+        self.buffer.clear()
         try:
             start = True
-            for tweet in ts.search_tweets_iterable(self.tso):
-                buffer.append(tweet)
+            count = 0
+            for tweet in self.ts.search_tweets_iterable(self.tso):
+                self.buffer.append(tweet)
+                current_amount_of_queries = self.ts.get_statistics()[0] #get current number of queries
                 if start:
-                    self.last_max_id =  tweet['id'] #get max tweet id for next searches
-                    print(self.last_max_id)
+                    self.setSinceId(tweet['id']) #get max tweet id for next searches
                     start = False
-            print(tweet['id'])
-            meta = ts.get_metadata()
+                if current_amount_of_queries == self.queriesAllowed:
+                    count += 1
+                    tweets_available = self.ts.get_amount_of_tweets()
+                    if count == tweets_available:
+                        break
+            meta = self.ts.get_metadata()
             reset_time = int(meta['x-rate-limit-reset'])
             secs = reset_time-int(time.time())
+            print("Tsq",self.dbKey,"complete")
             print("Queries remaining in current window: ",meta['x-rate-limit-remaining'])
             print("Time remaining: ",int(secs/60),"min ",secs%60,"sec")
         except TwitterSearchException as e:
-            if e.code == 429: #if query hit rate limit sleep for remaining time in window
+            if e.code == 429: #if query hit rate limit print remaining time in window
                 reset_time = int(ts.get_metadata()['x-rate-limit-reset'])
                 secs = reset_time-int(time.time())
                 #raise here maybe
-                print("Rate limit met - sleeping for ",int(secs/60),"min ",secs%60,"sec") #look into threads later
-                time.sleep(secs)
+                print("Rate limit met by tsq",self.dbKey, "- must wait for ",int(secs/60),"min ",secs%60,"sec")
             else:
-                print("Exception: %i - %s" % (e.code, e.message))
-                   
+                raise e
 
-tsq = TwitterSearchQuery(ts, tso)
-        
-    
 #Start of controller
 #-----
 class RestController:
-    def __init__(self, tsq):
-        self.buffer = ['default']
-        self.tsq = tsq
-        self.writer = []
+    def __init__(self, config):
+        self.config = config
+        self.ts = TwitterSearch(
+            consumer_key = config['TwitterAuth']['consumer_key'],
+            consumer_secret = config['TwitterAuth']['consumer_secret'],
+            access_token = config['TwitterAuth']['access_token'],
+            access_token_secret = config['TwitterAuth']['access_token_secret'])
+        self.writerList = []
+        self.tsqList = []
         
-    def basicSearch(self, keywordList):
-        self.tsq.setBasicSearchParameters(keywordList)
-        self.tsq.performSearch(self.buffer)
-        
-    def getBasicSearchParameters(self):
-        return self.tsq.getCurrentSearchURL()
+    def addNewTsqFromDatabaseDictionary(self, dictInput):
+        self.tsqList.append(TwitterSearchQuery(self.ts, dictInput, 0))
     
-    def firstTweet(self):
-        if (self.buffer):
-            return self.buffer[0]
+    # debug function
+    def basicSearchAll(self):
+        self._updateQueriesAllowed()
+        for tsq in self.tsqList:
+            if tsq.queriesAllowed == 0:
+                print("Not enough queries remaining")
+                break
+            tsq.performSearch()
+            
+    # debug function
+    def basicSearch(self, dbKey):
+        tsq = self._findTsqFromDbKey(dbKey)
+        if (tsq):
+            tsq.performSearch()
         
-    def updateConfig(self,config,path):
-        print(self.tsq.last_max_id)
-        config['TwitterSearchQuery']['last_max_id'] = str(self.tsq.last_max_id)
-        with open(path, 'w') as configfile:
-            config.write(configfile)   
+    # debug function
+    def getBasicSearchParameters(self, dbKey):
+        tsq = self._findTsqFromDbKey(dbKey)
+        if (tsq):
+            return tsq.getCurrentSearchURL()
+        else:
+            None
+    
+    # debug function
+    def firstTweet(self, dbKey):
+        tsq = self._findTsqFromDbKey(dbKey)
+        if (tsq):
+            return tsq.buffer[0]
+            
+    def _findTsqFromDbKey(self, dbKey):
+        for tsq in self.tsqList:
+            if (tsq.dbKey == dbKey):
+                return tsq
+        return None
+    
+    def _updateQueriesAllowed(self):
+        for tsq in self.tsqList:
+            try:
+                tsq.queriesAllowed = int(self.ts.get_metadata()['x-rate-limit-remaining']) / len(self.tsqList)
+            except TwitterSearchException as e:
+                if (e.code == 1012):
+                    tsq.queriesAllowed = 180 / len(self.tsqList)
+                else:
+                    raise e
+                    
+            # remove this when we are done experimenting
+            tsq.queriesAllowed /= 10
+            
 
         
-rController = RestController(tsq)
+rController = RestController(config)
 
-keywords = ['beer']
-rController.basicSearch(keywords)
-rController.updateConfig(config,'../bot.config')
+# Replace temp Dict with read from database
+tempDict = {'keywords' : ['beer'], 'location' : ['austin']}
+
+# add a new search query to the controller
+rController.addNewTsqFromDatabaseDictionary(tempDict);
+
+rController.basicSearchAll()
 
 #print(ts.get_metadata())
-print(rController.getBasicSearchParameters())
-print(rController.firstTweet())
+print(rController.getBasicSearchParameters(0))
+print(rController.firstTweet(0))
         
